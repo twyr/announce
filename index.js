fca86adcad82ce1828af6534e643e0cb86e192d4
Vish Desai <shadyvd@hotmail.com>
2021-01-12 17:46:10 +0530

    chore(version): bumped version to 0.2.0
    Bumped version, and added chalk + ora to the dependency list. This version will concentrate on
    better/cleaner console logging
    
    Signed-off-by: Vish Desai <shadyvd@hotmail.com>

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
  * @name		index.js
  * @file		Main entry point of the Twy'r Announce CLI tool
- * @version		0.1.0
+ * @version		0.2.0
  *
  * @author		Vish Desai <shadyvd@hotmail.com>
  * @copyright	(c) {@link https://twyr.github.io/annouce|Twy'r Announce} 2016-2020
