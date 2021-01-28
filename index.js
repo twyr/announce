f9359e78d6115ada57d00c13541ddb06eb09dbd1
Vish Desai <shadyvd@hotmail.com>
2021-01-28 18:08:13 +0530

    chore(version): bump version to 0.3.0
    Bumped version to tag that the release command uses Ora console logging now
    
    Signed-off-by: Vish Desai <shadyvd@hotmail.com>

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
  * @name		index.js
  * @file		Main entry point of the Twy'r Announce CLI tool
- * @version		0.2.0
+ * @version		0.3.0
  *
  * @author		Vish Desai <shadyvd@hotmail.com>
  * @copyright	(c) {@link https://twyr.github.io/annouce|Twy'r Announce} 2016-2020
