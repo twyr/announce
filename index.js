0bd76c59ea82d873ee7d13699c43dedfa5042be9
Vish Desai <shadyvd@hotmail.com>
2021-03-19 12:36:43 +0530

    chore(version): update to 0.7.2
    Signed-off-by: Vish Desai <shadyvd@hotmail.com>

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
  * @name		index.js
  * @file		Main entry point of the Twy'r Announce CLI tool
- * @version		0.7.1
+ * @version		0.7.2
  *
  * @author		Vish Desai <shadyvd@hotmail.com>
  * @copyright	(c) {@link https://twyr.github.io/annouce|Twy'r Announce} 2016-2021
